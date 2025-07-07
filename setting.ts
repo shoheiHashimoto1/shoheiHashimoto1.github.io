c1cc30a15d02c0ffbd9742baf94c928667f9fef1
shohei <itgoeson.619@gmail.com>
2025-07-07 14:13:06 +0900

    Add top.html for Progate tests and update TARGET_URL

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL = "https://shoheihashimoto1.github.io/";
